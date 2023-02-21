29c0433800f0621bf49570fe20f353114d9cd45f
Hunter Pearson <hjpearson2@gmail.com>
2023-02-20 21:17:54 -0700

    more site stuff

--- app.js
+++ app.js
@@ -19,16 +19,46 @@ mailchimp.setConfig({
 // Url for my audience list
 const url = "https://us13.api.mailchimp.com/3.0/lists/1b3730d610";
 
-// async function subscribeUser() {
-//     const response = await mailchimp.lists.addListMember(listId, {
-//       email_address: subscribingUser.email,
-//       status: "pending"
-//     });
-// }
-
 const listId = '1b3730d610';
 // const email = "hjpearson2@gmail.com";
 
+
+// First checks to see if a user is already subscribed. If they aren't, then they will be subscribed. If they are, then they will be notified.
+async function subscribeUser(subscriberEmail, subscriberHash, req, res) {
+  try {
+    const response = await mailchimp.lists.getListMember(listId, subscriberHash);
+    // console.log(`This user's subscription status is ${response.status}.`);
+    if (response.status == 'subscribed') {
+      req.flash('message', "That email is already subscribed to our list. If you aren't seeing our emails, check your promotions, spam, or junk folder.")
+      res.redirect('/'); 
+    }
+    else if (response.status == 'pending') {
+      req.flash('message', 'You have already signed up, but you have not officially opted in by verifying your email. Check your inbox, spam folder, or promotions folder to find our verification email.');
+      res.redirect('/');
+    }
+    else if (response.status == 'unsubscribed' || response.status == "archived") {
+      await mailchimp.lists.updateListMember(listId, subscriberHash, {
+      status: "subscribed"
+    });
+      res.redirect('welcome');
+    }
+  } 
+  catch (e) {
+    // Still need to test for the subcription of a completely new user. I am just waiting to have the redirected page done first *******************************************************************************
+    if (e.status === 404) {
+      await mailchimp.lists.addListMember(listId, {
+      email_address: subscriberEmail,
+      status: "pending"
+    });
+      console.log("Adding a completely new user");
+      // console.log(`Successfully added contact as an audience member. The contact's id is ${ sub_response.id }.`);
+      req.flash('email', `Welcome, for the first time, ${req.body.email}`);
+      res.redirect('welcome');
+    }
+  }
+}
+
+// Checks to see if a user is subscribed. If so, they will be granted access to the library. If not, they will be told to subscribe.
 async function validate(subscriberHash, req, res) {
   try {
     const response = await mailchimp.lists.getListMember(listId, subscriberHash);
@@ -37,9 +67,6 @@ async function validate(subscriberHash, req, res) {
       req.flash('email', req.body.email)
       res.redirect('library');
     }
-    // else if (response.status == 'pending') {
-      
-    // }
     else if (response.status == 'unsubscribed') {
       req.flash('failure', 'That email is not currently subscribed to Morning Chapter. Sign up on our home page to gain access to the library.');
       res.redirect('/library-restricted');
@@ -80,7 +107,25 @@ app.set('view engine', 'ejs');
 
 // Render the home page
 app.get('/', (req, res) => {
-  res.render('index');
+  const message = req.flash('message');
+  res.render('index', {
+    message: message
+  });
+});
+
+// Handle subscription from home page
+app.post('/', (req, res) =>  {
+  let userEmail = req.body.email;
+  const hash = md5(userEmail.toLowerCase());
+  subscribeUser(userEmail, hash, req, res);
+})
+
+// Render the welcome page
+app.get('/welcome', (req, res) => {
+  const email = req.flash('email');
+  res.render('welcome', {
+    email: email
+  });
 });
 
 // Handle form submission
@@ -105,4 +150,3 @@ app.get('/library', (req, res) => {
 });
 
 app.listen(port);
-
--- javascript/library.js
+++ javascript/library.js
@@ -1,22 +1,22 @@
 // When a book or login/sign up is clicked on, show the popup for it, dim the books and the background, and turn off pointer-events for all books
 function showPopup(id) {
     document.getElementById(id).style.display = "block";
-    document.body.style.background = "rgba(0, 0, 0, 0.5)";
-    document.getElementById('bg-image').style.filter= "brightness(50%)";
-    document.querySelector(".books img").style.filter = "brightness(50%)";
+    document.body.style.background = "linear-gradient(180deg, rgba(0,0,0,0.25) 25%, rgba(0,0,0,0.5) 60%, rgba(0,0,0,0.75) 90%)";
+    document.body.style.backgroundAttachment = "fixed";
+    // document.body.style.filter = "brightness(50%)";
+    document.querySelector(".books").style.filter = "brightness(50%)";
     document.querySelector(".books").style.pointerEvents = "none";
+    document.getElementById("heading-wrapper").style.filter = "brightness(50%)";
+    document.getElementById("nav").style.filter = "brightness(50%)";
 }
 
 // When the x is clicked, close the popup, return the background and images to normal, and allow books to be clicked again
 function closePopup(id) {
     document.getElementById(id).style.display = "none";
-    document.body.style.background = "#f5f3ff";
-    document.getElementById('bg-image').style.filter= "brightness(100%)";
-    document.querySelector(".books img").style.filter = "brightness(100%)";
+    document.body.style.background = "linear-gradient(180deg, rgba(245,243,255,1) 25%, rgba(184,175,255,1) 60%, rgba(90,74,227,1) 90%)";
+    document.body.style.backgroundAttachment = "fixed";
+    document.querySelector(".books").style.filter = "brightness(100%)";
     document.querySelector(".books").style.pointerEvents = "all";
-}
-
-// Return to home page when clicked
-function home() {
-    window.location.href = '/';
+    document.getElementById("heading-wrapper").style.filter = "brightness(100%)";
+    document.getElementById("nav").style.filter = "brightness(100%)";
 }
